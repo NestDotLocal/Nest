a86eb2768fce656863b3f2380d9f6114d6218080
TotoCode <totocodefr@duck.com>
2026-03-28 13:12:27 +0100

    create nest folder when it doesn't exist

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,9 @@
+import path from "node:path"
+import fs from "node:fs"
+
+if (!fs.existsSync(path.resolve(__dirname, "nest"))) {
+    fs.mkdirSync(path.resolve(__dirname, "nest"))
+}
+
 import "./server/main";
 import "./db/main";
